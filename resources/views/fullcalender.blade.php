b8d28bad19c041968cb2b5bf22b471c7fce8156a
Marcos Ronaldo <marcos.ronaldo@gmail.com>
2024-04-22 22:05:33 -0300

    Ajustes visuais no calendário

--- resources/views/fullcalender.blade.php
+++ resources/views/fullcalender.blade.php
@@ -126,7 +126,8 @@
             dayMaxEvents: true, // allow "more" link when too many events
     
             
-            events: "{{env('APP_URL')}}/calendario/list",
+           //events: "{{env('APP_URL')}}/calendario/list",
+           events: 'http://rikassa.exatamentepublicidade.com.br/calendario/list',
             
             
             eventClick: function (info) {
