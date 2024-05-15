8d639219cd78313d5ebdd67596f7651acd8ccb44
Marcos Ronaldo <marcos.ronaldo@gmail.com>
2024-05-14 22:36:52 -0300

    Adicionado uma exceção no middlweware

--- routes/web.php
+++ routes/web.php
@@ -62,10 +62,10 @@ Route::middleware(['cors'])->group(function () {
 });
 
 
-Route::post('mpwebhook', [InscricaoController::class, 'mpwebhook'])->name('mpwebhook');
-// Route::post('/mercadopago/mpwebhook', function (Request $request) {
-//     Log::debug($request->input());
-// });
+// Route::post('mpwebhook', [InscricaoController::class, 'mpwebhook'])->name('mpwebhook');
+Route::post('mpwebhook', function (Request $request) {
+    Log::debug($request->input());
+});
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
