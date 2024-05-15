237743c63a206bb12f5a312d98803d90b8cacadb
Marcos Ronaldo <marcos.ronaldo@gmail.com>
2024-05-14 21:53:08 -0300

    Correção da rota do do webrook

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InscricaoController;
 
@@ -14,7 +15,11 @@ use App\Http\Controllers\InscricaoController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::post('/mercadopago/mpwebhook', [InscricaoController::class, 'mpwebhook'])->name('mpwebhook');
+// Route::post('/mercadopago/mpwebhook', [InscricaoController::class, 'mpwebhook'])->name('mpwebhook');
+Route::post('/mercadopago/mpwebhook', function (Request $request) {
+    Log::debug($request->input());
+})->name('payment.update');
+
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
