30b3a360d0ba1cb07350d2b7139fb6c09f5a3381
Marcos Ronaldo <marcos.ronaldo@gmail.com>
2024-05-18 22:40:15 -0300

    Configurado NI e SIGNATURE no env e controller

--- app/Http/Controllers/AdmEventosController.php
+++ app/Http/Controllers/AdmEventosController.php
@@ -57,7 +57,7 @@ class AdmEventosController extends Controller
             Auth::login($user);
         }
 
-        return view('adm.calendario.listar', compact('user'));
+        return view('adm.calendario.listar');
     }
 
 
